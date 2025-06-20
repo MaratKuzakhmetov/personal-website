004975a5ca6fdf039f6cc18f3c3e35501e3a6e98
Marat Kuzakhmetov <kuzakhmetovmarat@gmail.com>
2025-06-20 17:15:25 +0200

    fixed styles in Footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -15,15 +15,15 @@ export const Footer: React.FC<FooterProps> = ({ data }) => {
   return (
     <footer className={styles.root}>
       <div className={styles.footer}>
-        <div>
+        <div className={styles.contacts}>
           {data.socialLinks &&
             data.socialLinks.map(link => (
-              <Link key={link.platform} href={link.url}>
+              <Link key={link.platform} href={link.url} target="_blank">
                 {link.platform}
               </Link>
             ))}
         </div>
-        <div>
+        <div className={styles.copyright}>
           <p className={styles.footerText}>{copyright}</p>
         </div>
       </div>

68bb9c38d14ca024d747e9d0998f39a20c1e8635
Marat Kuzakhmetov <kuzakhmetovmarat@gmail.com>
2025-10-18 21:13:14 +0200

    added cache logic

---
+++ functions/sanity-images/[...path].ts
@@ -0,0 +1,26 @@
+/// <reference types="@cloudflare/workers-types" />
+
+export const onRequest: PagesFunction = async context => {
+  const { request, params, env } = context;
+  const { path } = params as { path: string };
+
+  const sanityBase = `https://cdn.sanity.io/images/${env.SANITY_PROJECT_ID}/${env.SANITY_DATASET}`;
+  const sanityUrl = `${sanityBase}/${path}`;
+
+  const cache = (caches as CacheStorage & { default: Cache }).default;
+  const cacheKey = new Request(request.url, request);
+  const cached = await cache.match(cacheKey);
+
+  if (cached) return cached;
+
+  const res = await fetch(sanityUrl, {
+    headers: { 'User-Agent': 'Cloudflare Worker Image Proxy' },
+  });
+
+  const response = new Response(res.body, res);
+  response.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
+
+  context.waitUntil(cache.put(cacheKey, response.clone()));
+
+  return response;
+};

ca21ffbc6f3eae285261f7760703e7f35c639b39
Marat Kuzakhmetov <kuzakhmetovmarat@gmail.com>
2025-10-27 13:27:26 +0100

    removed main page from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,6 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://markgp.com/',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1.0,
-    },
-    {
       url: 'https://markgp.com/en',
       lastModified: new Date(),
       changeFrequency: 'monthly',
